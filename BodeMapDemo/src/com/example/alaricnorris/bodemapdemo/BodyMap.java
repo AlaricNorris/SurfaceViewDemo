64d00e1415d0fe95149481db88204e27d721df4f
AlaricNorris <norris.sly@gmail.com>
2015-06-13 17:12:25 +0800

    ArrayIndexOutofBounds

--- BodeMapDemo/src/com/example/alaricnorris/bodemapdemo/BodyMap.java
+++ BodeMapDemo/src/com/example/alaricnorris/bodemapdemo/BodyMap.java
@@ -11,9 +11,6 @@ package com.example.alaricnorris.bodemapdemo ;
 
 import java.util.ArrayList ;
 import java.util.HashMap ;
-import java.util.HashSet ;
-import java.util.Map ;
-import java.util.Set ;
 import android.content.Context ;
 import android.content.res.TypedArray ;
 import android.graphics.Bitmap ;
@@ -25,10 +22,9 @@ import android.graphics.Paint.Style ;
 import android.graphics.Path ;
 import android.graphics.Point ;
 import android.graphics.Rect ;
-import android.graphics.RectF ;
 import android.graphics.Region ;
 import android.graphics.RegionIterator ;
-import android.os.Handler ;
+import android.text.TextUtils ;
 import android.util.AttributeSet ;
 import android.util.Log ;
 import android.view.MotionEvent ;
@@ -58,15 +54,6 @@ public class BodyMap extends ImageView {
 
 	private Context mContext ;
 
-	//保存所有热点区域
-	private Map<String , MapArea> mMapArea ;
-
-	//保存点击的区域
-	private Set<String> mFocus ;
-
-	//点击时Path区域的转换，用于触摸点的判断
-	protected RectF mPathRectF = new RectF() ;
-
 	public static final int MIN_WIDTH = 320 ;
 
 	public static final int MIN_HEIGHT = 480 ;
@@ -78,25 +65,18 @@ public class BodyMap extends ImageView {
 	private int mHeight = 0 ;
 
 	/**
-	 * 	多边形颜色
-	 * 	int			:		mPolygenColor	
+	 * 	检测区域颜色
+	 * 	int			:		mDetectRegionColor	
 	 * 	@since Ver 1.0
 	 */
-	private int mPolygenColor ;
+	private int mDetectRegionColor ;
 
 	/**
-	 * 	内边距
-	 * 	int			:		mInnerMargin	
+	 * 	是否绘制检测区域
+	 * 	boolean			:		showDetectRegion	
 	 * 	@since Ver 1.0
 	 */
-	private int mInnerMargin ;
-
-	/**
-	 * 	是否绘制多边形
-	 * 	boolean			:		showPolygen	
-	 * 	@since Ver 1.0
-	 */
-	private boolean showPolygen ;
+	private boolean showDetectRegion ;
 
 	/**
 	 * 	人体图绘制的矩形范围
@@ -112,7 +92,7 @@ public class BodyMap extends ImageView {
 	 * 	Bitmap			:		mImage	
 	 * 	@since Ver 1.0
 	 */
-	private Bitmap mImage ;
+	private Bitmap mBitmap ;
 
 	/**
 	 *	mImage
@@ -120,7 +100,7 @@ public class BodyMap extends ImageView {
 	 */
 	public void setImage(Bitmap mImage) {
 		//TODO
-		this.mImage = mImage ;
+		this.mBitmap = mImage ;
 		requestLayout() ;
 	}
 
@@ -133,6 +113,9 @@ public class BodyMap extends ImageView {
 	public BodyMap(Context context , AttributeSet attrs , int defStyle) {
 		super(context , attrs , defStyle) ;
 		mContext = context ;
+		if(isInEditMode()) {
+			return ;
+		}
 		TypedArray mTypedArray = context.getTheme().obtainStyledAttributes(attrs ,
 				R.styleable.BodyMap , defStyle , 0) ;
 		int n = mTypedArray.getIndexCount() ;
@@ -140,35 +123,71 @@ public class BodyMap extends ImageView {
 			int attrIndex = mTypedArray.getIndex(i) ;
 			switch(attrIndex) {
 				case R.styleable.BodyMap_image :
-					mImage = BitmapFactory.decodeResource(getResources() ,
+					mBitmap = BitmapFactory.decodeResource(getResources() ,
 							mTypedArray.getResourceId(attrIndex , 0)) ;
 					break ;
-				case R.styleable.BodyMap_showPolygon :
-					showPolygen = mTypedArray.getBoolean(attrIndex , true) ;
+				case R.styleable.BodyMap_showdetectRegion :
+					showDetectRegion = mTypedArray.getBoolean(attrIndex , false) ;
 					break ;
-				case R.styleable.BodyMap_PolygonColor :
+				case R.styleable.BodyMap_detectRegionColor :
 					// 默认颜色设置为黑色  
-					mPolygenColor = mTypedArray.getColor(attrIndex , Color.LTGRAY) ;
+					mDetectRegionColor = mTypedArray.getColor(attrIndex , Color.LTGRAY) ;
 					break ;
-				case R.styleable.BodyMap_innerMargin :
-					mInnerMargin = mTypedArray.getDimensionPixelSize(attrIndex ,
-							DEAULT_INNER_MARGIN) ;
+				case R.styleable.BodyMap_imagesLayers :
+					mImageLayersNames = getResources().getStringArray(
+							mTypedArray.getResourceId(attrIndex , 0)) ;
+					break ;
+				case R.styleable.BodyMap_XCoordinates :
+					XCoordinates = getResources().getIntArray(
+							mTypedArray.getResourceId(attrIndex , 0)) ;
+					break ;
+				case R.styleable.BodyMap_YCoordinates :
+					YCoordinates = getResources().getIntArray(
+							mTypedArray.getResourceId(attrIndex , 0)) ;
 					break ;
 			}
 		}
 		mTypedArray.recycle() ;
+		Log.i("tag" + getClass().getSimpleName() , "mImageLayersNames:" + mImageLayersNames) ;
 		mPaint = new Paint() ;
-		mPaint.setColor(mPolygenColor) ;
+		mPaint.setColor(mDetectRegionColor) ;
 		mPaint.setStyle(Style.FILL) ;
 		mRect_Bound = new Rect() ;
-		initDatas() ;
+		mPoints = new ArrayList<Point>() ;
+		if(XCoordinates != null && XCoordinates.length > 0) {
+			for(int i = 0 ; i < XCoordinates.length ; i ++ ) {
+				mPoints.add(new Point(XCoordinates[i] , YCoordinates[i])) ;
+			}
+		}
+		try {
+			if(mImageLayersNames != null && mImageLayersNames.length > 0) {
+				for(int i = 0 ; i < mImageLayersNames.length ; i ++ ) {
+					mHashMap.put(
+							mImageLayersNames[i] ,
+							BitmapFactory.decodeResource(
+									getResources() ,
+									mTypedArray.getResourceId(
+											getResources().getIdentifier(mImageLayersNames[i] ,
+													"drawable" , mContext.getPackageName()) , 0))) ;
+				}
+			}
+		}
+		catch(Exception e) {
+			Log.e("tag" , "Exception" + e.toString()) ;
+			e.printStackTrace() ;
+		}
+		Log.i("tag" , "mHashMap" + mHashMap) ;
 	}
 
-	private void initDatas() {
-		mMapArea = new HashMap<String , MapArea>() ;
-		mFocus = new HashSet<String>() ;
-		initMapArea() ;
-	}
+	private String[] mImageLayersNames ;
+
+	private HashMap<String , Bitmap> mHashMap ;
+
+	private String choosedImageName ;
+
+	private int[] XCoordinates ;
+
+	private int[] YCoordinates ;
 
 	/**
 	 * 	Creates a new instance of BodyMayImageView.
@@ -215,17 +234,63 @@ public class BodyMap extends ImageView {
 	 */
 	@ Override
 	protected void onDraw(Canvas canvas) {
+//		drawBody(canvas) ;
+		super.onDraw(canvas) ;
+//		if(isInEditMode()) {
+//			return ;
+//		}
 		try {
-			customDraw(canvas) ;
+			drawDetectRegion(canvas) ;
 		}
 		catch(Exception e) {
 			e.printStackTrace() ;
 		}
-		super.onDraw(canvas) ;
+		drawChoosedLayer(canvas) ;
 	}
 
 	/**
-	 * 	customDraw:()
+	 * 	drawChoosedLayer:()
+	 *  ──────────────────────────────────
+	 * 	@param canvas	
+	 *	@version	Ver 1.0	
+	 * 	@since  	I used to be a programmer like you, then I took an arrow in the knee　
+	 *	──────────────────────────────────────────────────────────────────────────────────────────────────────
+	 * 	Modified By 	20144L151		 2015-6-13下午4:32:12
+	 *	Modifications:	TODO
+	 *	──────────────────────────────────────────────────────────────────────────────────────────────────────
+	 */
+	private void drawChoosedLayer(Canvas canvas) {
+		if(TextUtils.isEmpty(choosedImageName)) {
+			return ;
+		}
+		canvas.drawBitmap(mHashMap.get(choosedImageName) , super.getImageMatrix() , mPaint) ;
+	}
+
+	/**
+	 * 	drawBody:(画人体图)
+	 *  ──────────────────────────────────
+	 * 	@param canvas	
+	 *	@version	Ver 1.0	
+	 * 	@since  	I used to be a programmer like you, then I took an arrow in the knee　
+	 *	──────────────────────────────────────────────────────────────────────────────────────────────────────
+	 * 	Modified By 	20144L151		 2015-6-13下午2:38:32
+	 *	Modifications:	init
+	 *	──────────────────────────────────────────────────────────────────────────────────────────────────────
+	 */
+	private void drawBody(Canvas canvas) {
+		mPaint.setStyle(Paint.Style.FILL) ;//设置填满  
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources() , R.drawable.body_male_front) ;
+//		mRect_Bound.left = mWidth / 2 - mImage.getWidth() / 2 ;
+//		mRect_Bound.right = mWidth / 2 + mImage.getWidth() / 2 ;
+//		mRect_Bound.top = (mHeight) / 2 - mImage.getHeight() / 2 ;
+//		mRect_Bound.bottom = (mHeight) / 2 + mImage.getHeight() / 2 ;
+		mPaint.setColor(mDetectRegionColor) ;
+		canvas.drawRect(mRect_Bound , mPaint) ;
+		canvas.drawBitmap(bitmap , null , mRect_Bound , mPaint) ;
+	}
+
+	/**
+	 * 	customDraw:(绘制检测区域)
 	 *  ──────────────────────────────────
 	 * 	@param canvas	
 	 *	@version	Ver 1.0	
@@ -235,21 +300,7 @@ public class BodyMap extends ImageView {
 	 *	Modifications:	TODO
 	 *	──────────────────────────────────────────────────────────────────────────────────────────────────────
 	 */
-	private void customDraw(Canvas canvas) {
-		mPaint.setStyle(Paint.Style.FILL) ;//设置填满  
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources() , R.drawable.body_male_front) ;
-//		mRect_Bound.left = mWidth / 2 - mImage.getWidth() / 2 ;
-//		mRect_Bound.right = mWidth / 2 + mImage.getWidth() / 2 ;
-//		mRect_Bound.top = (mHeight) / 2 - mImage.getHeight() / 2 ;
-//		mRect_Bound.bottom = (mHeight) / 2 + mImage.getHeight() / 2 ;
-		mPaint.setColor(mPolygenColor) ;
-		canvas.drawRect(mRect_Bound , mPaint) ;
-		canvas.drawBitmap(bitmap , null , mRect_Bound , mPaint) ;
-		//进行触摸区域绘制
-//		for(String key : mFocus) {
-//			Path path = mMapArea.get(key).getPath() ;
-//			canvas.drawPath(path , mPaint) ;
-//		}
+	private void drawDetectRegion(Canvas canvas) {
 		Path mPath = new Path() ;
 		for(int i = 0 ; i < mPoints.size() ; i ++ ) {
 			if(i == 0) {
@@ -260,9 +311,12 @@ public class BodyMap extends ImageView {
 				mPath.lineTo(mPoints.get(0).x , mPoints.get(0).y) ;
 			}
 		}
-		mRegion.setPath(mPath , new Region(0 , 0 , mImage.getWidth() , mImage.getHeight())) ;
-		mPaint.setARGB(222 , 88 , 88 , 88) ;
-		drawRegion(canvas , mRegion , mPaint) ;
+		mPath.transform(super.getImageMatrix()) ;
+		mRegion.setPath(mPath , new Region(0 , 0 , getWidth() , getHeight())) ;
+		mPaint.setColor(mDetectRegionColor) ;
+		if(showDetectRegion) {
+			drawRegion(canvas , mRegion , mPaint) ;
+		}
 	}
 
 	Region mRegion = new Region() ;
@@ -278,83 +332,62 @@ public class BodyMap extends ImageView {
 		}
 	}
 
-	@ Override
-	protected void onMeasure(int widthMeasureSpec , int heightMeasureSpec) {
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec) ;
-		int widthSize = MeasureSpec.getSize(widthMeasureSpec) ;
-		if(widthMode == MeasureSpec.EXACTLY) {
-			Log.i(getClass().getName() , "width EXACTLY") ;
-			mWidth = widthSize ;
-		}
-		else {
-			//TODO Rect
-			if(mImage == null)
-				mWidth = (int) (getPaddingLeft() + mInnerMargin * 2 + getPaddingRight()) ;
-			else
-//				mWidth = (int) (getPaddingLeft() + mImage.getWidth() + mInnerMargin * 2 + getPaddingRight()) ;
-				mWidth = mImage.getWidth() ;
-		}
-		int heightMode = MeasureSpec.getMode(heightMeasureSpec) ;
-		int heightSize = MeasureSpec.getSize(heightMeasureSpec) ;
-		if(heightMode == MeasureSpec.EXACTLY) {
-			Log.i(getClass().getName() , "height EXACTLY") ;
-			mHeight = heightSize ;
-		}
-		else {
-			//TODO Rect
-			if(mImage == null)
-				mHeight = (int) (getPaddingTop() + mInnerMargin * 2 + getPaddingBottom()) ;
-			else
-//				mHeight = (int) (getPaddingTop() + mImage.getHeight() + mInnerMargin * 2 + getPaddingBottom()) ;
-				mHeight = mImage.getHeight() ;
-		}
-		mRect_Bound.left = 0 ;
-		mRect_Bound.right = mWidth ;
-		mRect_Bound.top = 0 ;
-		mRect_Bound.bottom = mHeight ;
-//		mRect_Bound.left = getPaddingLeft() + mInnerMargin ;
-//		mRect_Bound.right = mWidth - getPaddingRight() - mInnerMargin ;
-//		mRect_Bound.top = getPaddingTop() + mInnerMargin ;
-//		mRect_Bound.bottom = mHeight - getPaddingBottom() - mInnerMargin ;
-		setMeasuredDimension(mWidth , mHeight) ;
-		ScaleDegreeX = mRect_Bound.width() / mWidth ;
-		ScaleDegreeY = mRect_Bound.height() / mHeight ;
-		initMapArea() ;
-	}
-
+//	@ Override
+//	protected void onMeasure(int widthMeasureSpec , int heightMeasureSpec) {
+//		int widthMode = MeasureSpec.getMode(widthMeasureSpec) ;
+//		int widthSize = MeasureSpec.getSize(widthMeasureSpec) ;
+//		if(widthMode == MeasureSpec.EXACTLY) {
+//			Log.i(getClass().getName() , "width EXACTLY") ;
+//			mWidth = widthSize ;
+//		}
+//		else {
+//			//TODO Rect
+//			if(mImage == null)
+//				mWidth = (int) (getPaddingLeft() + mInnerMargin * 2 + getPaddingRight()) ;
+//			else
+////				mWidth = (int) (getPaddingLeft() + mImage.getWidth() + mInnerMargin * 2 + getPaddingRight()) ;
+//				mWidth = mImage.getWidth() ;
+//		}
+//		int heightMode = MeasureSpec.getMode(heightMeasureSpec) ;
+//		int heightSize = MeasureSpec.getSize(heightMeasureSpec) ;
+//		if(heightMode == MeasureSpec.EXACTLY) {
+//			Log.i(getClass().getName() , "height EXACTLY") ;
+//			mHeight = heightSize ;
+//		}
+//		else {
+//			//TODO Rect
+//			if(mImage == null)
+//				mHeight = (int) (getPaddingTop() + mInnerMargin * 2 + getPaddingBottom()) ;
+//			else
+////				mHeight = (int) (getPaddingTop() + mImage.getHeight() + mInnerMargin * 2 + getPaddingBottom()) ;
+//				mHeight = mImage.getHeight() ;
+//		}
+//		mRect_Bound.left = 0 ;
+//		mRect_Bound.right = mWidth ;
+//		mRect_Bound.top = 0 ;
+//		mRect_Bound.bottom = mHeight ;
+////		mRect_Bound.left = getPaddingLeft() + mInnerMargin ;
+////		mRect_Bound.right = mWidth - getPaddingRight() - mInnerMargin ;
+////		mRect_Bound.top = getPaddingTop() + mInnerMargin ;
+////		mRect_Bound.bottom = mHeight - getPaddingBottom() - mInnerMargin ;
+//		setMeasuredDimension(mWidth , mHeight) ;
+//		ScaleDegreeX = mRect_Bound.width() / mWidth ;
+//		ScaleDegreeY = mRect_Bound.height() / mHeight ;
+//	}
 	@ Override
 	public boolean onTouchEvent(MotionEvent event) {
+		// TODO 根据事件来响应不同feedback
 		Log.i("tag" , "onTouchEvent" + event.toString()) ;
 		Log.i("tag" , "xy" + event.getX() + event.getY()) ;
-//		if(mHandler != null) {
-//			checkAreas(event) ;
-//			if( ! mFocus.isEmpty()) {
-//				MapArea area = null ;
-//				for(String key : mFocus) {
-//					area = mMapArea.get(key) ;
-//					invalidate() ;
-//					mHandler.obtainMessage(0 , area.getPtKeys()).sendToTarget() ;
-//				}
-//			}
-//		}
 		if(mRegion.contains((int) event.getX() , (int) event.getY())) {
-			Log.i("tag" , "yeah" + event.getX() + event.getY()) ;
-			Toast.makeText(getContext() , "yeah" , 0).show() ;
+			Log.i("tag" , "yeah!!!!!!!!") ;
+			choosedImageName = mImageLayersNames[0] ;
 		}
-		return super.onTouchEvent(event) ;
-	}
-
-	private void checkAreas(MotionEvent event) {
-		mFocus.clear() ;
-		for(String key : mMapArea.keySet()) {
-			mPathRectF.setEmpty() ;
-			Path path = mMapArea.get(key).getPath() ;
-			path.computeBounds(mPathRectF , true) ;
-			if(mPathRectF.contains(event.getX() , event.getY())) {
-				mFocus.add(key) ;
-				break ;
-			}
+		else {
+			choosedImageName = null ;
 		}
+		invalidate() ;
+		return true ;
 	}
 
 	private float ScaleDegreeX ;
@@ -365,74 +398,5 @@ public class BodyMap extends ImageView {
 	private float toDip(Context context , float pxValue) {
 		final float scale = context.getResources().getDisplayMetrics().density ;
 		return (int) (pxValue * scale + 0.5f) ;
-	}
-
-	public void initMapArea() {
-		mMapArea.clear() ;
-		mFocus.clear() ;
-		String[] keys = mContext.getResources().getStringArray(R.array.map_area) ;
-		MapArea bodyArea = null ;
-		int idenId = - 1 ;
-		if(keys != null) {
-			for(String key : keys) {
-				idenId = mContext.getResources().getIdentifier(key , "array" ,
-						mContext.getPackageName()) ;
-				int[] paths = mContext.getResources().getIntArray(idenId) ;
-				idenId = mContext.getResources().getIdentifier(key + "_code" , "array" ,
-						mContext.getPackageName()) ;
-				String[] ptKeys = mContext.getResources().getStringArray(idenId) ;
-				bodyArea = new MapArea(ptKeys , paths) ;
-				mMapArea.put(key , bodyArea) ;
-			}
-		}
-	}
-
-	private Handler mHandler = new Handler() ;
-
-	public void setHandler(Handler handler) {
-		this.mHandler = handler ;
-	}
-
-	//区域对象
-	class MapArea {
-
-		private String[] mPtKeys ;
-
-		private Path mPath ;
-
-		public MapArea(String[] ptKeys , int[] paths) {
-			super() ;
-			this.mPtKeys = ptKeys ;
-			mPath = new Path() ;
-			int len = paths.length ;
-			//每两个点做一个坐标
-			for(int i = 0 ; i < len ; i = i + 2) {
-				if(i == 0) {
-					mPath.moveTo(convertLocation(toDip(mContext , paths[i])) ,
-							convertLocation(toDip(mContext , paths[i + 1]))) ;
-				}
-				else {
-					mPath.lineTo(convertLocation(toDip(mContext , paths[i])) ,
-							convertLocation(toDip(mContext , paths[i + 1]))) ;
-				}
-			}
-			mPath.close() ;
-		}
-
-		public String[] getPtKeys() {
-			return mPtKeys ;
-		}
-
-		public void setPtKeys(String[] ptKeys) {
-			this.mPtKeys = ptKeys ;
-		}
-
-		public Path getPath() {
-			return mPath ;
-		}
-
-		protected float convertLocation(float inLocation) {
-			return inLocation + mInnerMargin ;
-		}
 	}
 }
